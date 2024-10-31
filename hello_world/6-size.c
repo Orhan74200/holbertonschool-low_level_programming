918f9de330c811361b56934e6258c6a8463df6d6
Orhan74200 <Orhan74200@icloud.com>
2024-10-31 10:00:48 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -8,6 +14,5 @@ printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 printf("Size of a double: %lu byte(s)\n", sizeof(double));
-
 return (0);
 }
