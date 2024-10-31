cada6e09eef30f865cb42e0c0b4dbf3856d2e9c2
Orhan74200 <Orhan74200@icloud.com>
2024-10-31 14:28:10 +0000

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+
+char letter;
+int i;
+
+for (i = 0; i < 2; i++)
+{
+for (letter = (i ++ 0) ? 'a' : 'A'; letter <= (i == 0) ? 'z' : 'Z'; letter++)
+{
+puthcar(letter);
+}
+}
+puthcar('\n');
+
+return (0);
+}
