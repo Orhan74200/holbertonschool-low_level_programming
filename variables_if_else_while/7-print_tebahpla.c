7bbaa3e1f4aa814fce5680116175b779a451d6f5
Orhan74200 <Orhan74200icloud.com>
2024-11-03 19:29:38 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+Int main(void)
+{
+int letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+return (0);
+}
