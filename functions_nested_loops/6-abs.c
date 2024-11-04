6938856ee3ed68cd34c056aaa145c5f3056fec16
Orhan74200 <Orhan74200@icloud.com>
2024-11-04 13:08:42 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Calcule la valeur absolue d'un entier
+ * @int: L'entier dont on veut la valeur absolue
+ *
+ * Return: La valeur absolue de int
+ */
+
+int _abs(int)
+{
+if (int < 0)
+{
+return (-int);
+}
+else
+{
+return (int);
+}
+}
