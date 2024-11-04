b88a65dbfcb03360180f671996bce529287165ed
Orhan74200 <Orhan74200@icloud.com>
2024-11-04 15:02:37 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -16,15 +16,10 @@ int print_last_digit(int n)
 int last_digit = n % 10;
 
 if (last_digit < 0)
-{
+
 last_digit = -last_digit;
-}
 
-int buffer[2];
+_putchar(last_digit + '0');
 
-buffer[0] = last_digit + '0';
-buffer[1] = '\0';
-
-write(1, buffer, 1);
 return (last_digit);
 }
