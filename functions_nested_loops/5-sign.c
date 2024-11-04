f12e50cfa1bd509ad9199279610bb0196aa498a6
Orhan74200 <Orhan74200@icloud.com>
2024-11-04 13:02:13 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -18,17 +18,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
+write(1, '+', 1);
 return (1);
 }
 else if (n == 0)
 {
-printf("0");
+write(1, '0', 1);
 return (0);
 }
 else
 {
-printf("-");
+write(1, '-', 1);
 return (-1);
 }
 }
