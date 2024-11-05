59b1908c240034e018001afe5457c147ca5ab3bc
Orhan74200 <Orhan74200>
2024-11-05 10:00:17 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+int add(int, int)
+{
+
+int result = add(5, 3);
+printf("5 + 3 = %d\n", result);
+return (0);
+}
+
+int add(int a, int b)
+{
+return (a + b);
+}
+}
